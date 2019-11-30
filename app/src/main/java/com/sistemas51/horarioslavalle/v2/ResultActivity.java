99679ee4cd4fe85cd835992722546155b710e56f
Axel Fernandez <axel.fernandez@sitrack.com>
2019-11-30 00:37:06 -0300

    See the fucking time of the fucking push, i'm fucking drunk!

---
+++ app/src/main/java/com/sistemas51/horarioslavalle/v2/ResultActivity.java
@@ -0,0 +1,80 @@
+package com.sistemas51.horarioslavalle.v2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.sistemas51.horarioslavalle.R;
+
+public class ResultActivity extends AppCompatActivity {
+    Fragment active;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        Bundle bundleWeek = getIntent().getExtras();
+        bundleWeek.putString(getResources().getString(R.string.Type),getResources().getString(R.string.Semana));
+        Bundle bundleSaturday = getIntent().getExtras();
+        bundleSaturday.putString(getResources().getString(R.string.Type),getResources().getString(R.string.Sabado));
+        Bundle bundleSunday = getIntent().getExtras();
+        bundleSunday.putString(getResources().getString(R.string.Type),getResources().getString(R.string.Domingo));
+
+
+        final Fragment week = new ResultFragment();
+        week.setArguments(bundleWeek);
+        final Fragment saturday = new ResultFragment();
+        saturday.setArguments(bundleSaturday);
+        final Fragment sunday = new ResultFragment();
+        sunday.setArguments(bundleSunday);
+        final FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().add(R.id.container, sunday, "3").hide(sunday).commit();
+        fm.beginTransaction().add(R.id.container, saturday, "2").hide(saturday).commit();
+        fm.beginTransaction().add(R.id.container,week, "1").commit();
+
+        active = week;
+
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+
+                    case R.id.navigation_week:
+                        fm.beginTransaction().hide(active).show(week).commit();
+                        active = week;
+                        return true;
+
+                    case R.id.navigation_saturday:
+                        fm.beginTransaction().hide(active).show(saturday).commit();
+                        active = saturday;
+                        return true;
+
+                    case R.id.navigation_sunday:
+                        fm.beginTransaction().hide(active).show(sunday).commit();
+                        active = sunday;
+                        return true;
+                }
+                return false;
+
+
+                }
+
+        });
+
+
+    }
+
+
+
+}
