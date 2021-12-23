0a7b9b59502d8d99e9be32f831bb64d1415a05f4
AxelFernandez <axel.fernandez0145@gmail.com>
2021-12-22 22:29:34 -0300

    Fixing Special Hours

--- app/src/main/java/com/sistemas51/horarioslavalle/adapters/StepperRvAdapter.java
+++ app/src/main/java/com/sistemas51/horarioslavalle/adapters/StepperRvAdapter.java
@@ -29,30 +29,31 @@ public class StepperRvAdapter extends RecyclerView.Adapter<StepperRvAdapter.View
     int currentStep;
     boolean special;
 
-    public StepperRvAdapter(Context context, List<String> selectedStepper, int currentStep,  Map<String, String> hourSelected){
+    public StepperRvAdapter(Context context, List<String> selectedStepper, int currentStep, Map<String, String> hourSelected) {
         this.context = context;
         this.selectedStepper = selectedStepper;
         this.currentStep = currentStep;
         this.hourSelected = hourSelected;
     }
 
-    public StepperRvAdapter(Context context, List<String> selectedStepper, int currentStep,boolean special, Map<String, String> hourSelected){
+    public StepperRvAdapter(Context context, List<String> selectedStepper, int currentStep, boolean special, Map<String, String> hourSelected) {
         this.context = context;
         this.selectedStepper = selectedStepper;
         this.currentStep = currentStep;
         this.special = special;
         this.hourSelected = hourSelected;
     }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_step_selected,viewGroup,false));
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_step_selected, viewGroup, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull final ViewHolder viewHolder, final int i) {
         viewHolder.description.setText(selectedStepper.get(i));
-        if (currentStep == 2 && selectedStepper.get(i).equals(hourSelected.get(context.getResources().getString(R.string.from)))){
+        if (currentStep == 2 && selectedStepper.get(i).equals(hourSelected.get(context.getResources().getString(R.string.from)))) {
             viewHolder.cardView.setBackgroundColor(Color.parseColor("#fb8c00"));
             viewHolder.description.setTextColor(Color.parseColor("#ffffff"));
         }
@@ -61,23 +62,19 @@ public class StepperRvAdapter extends RecyclerView.Adapter<StepperRvAdapter.View
             @Override
             public void onClick(View view) {
                 if (currentStep == 0 && i == 5) {
-                    if (special) {
-                        Navigation.findNavController(view).navigate(R.id.to_specialHours_action);
-                    }else{
-                        Snackbar.make(view, "No hay horarios especiales ahora, intenta nuevamente mas tarde", Snackbar.LENGTH_LONG).show();
-                    }
-                }else if(currentStep == 0 && i !=5) {
+                    Navigation.findNavController(view).navigate(R.id.to_specialHours_action);
+                } else if (currentStep == 0 && i != 5) {
                     hourSelected.put(context.getResources().getString(R.string.route), selectedStepper.get(i));
                     OriginSelectedDirections.NextAction action = OriginSelectedDirections.nextAction((HashMap) hourSelected);
                     Navigation.findNavController(view).navigate(action);
-                }else if(currentStep == 1){
+                } else if (currentStep == 1) {
                     hourSelected.put(context.getResources().getString(R.string.from), selectedStepper.get(i));
                     OriginSelectedDirections.NextAction action = OriginSelectedDirections.nextAction((HashMap) hourSelected);
                     Navigation.findNavController(view).navigate(action);
-                }else if(currentStep == 2){
-                    if (selectedStepper.get(i).equals(hourSelected.get("from"))){
+                } else if (currentStep == 2) {
+                    if (selectedStepper.get(i).equals(hourSelected.get("from"))) {
                         Snackbar.make(view, "No es posible Seleccionar el mismo origen y destino, selecciona otro", Snackbar.LENGTH_LONG).show();
-                    }else{
+                    } else {
                         hourSelected.put(context.getResources().getString(R.string.to), selectedStepper.get(i));
                         OriginSelectedDirections.NextAction action = OriginSelectedDirections.nextAction((HashMap) hourSelected);
                         Navigation.findNavController(view).navigate(action);
@@ -96,20 +93,21 @@ public class StepperRvAdapter extends RecyclerView.Adapter<StepperRvAdapter.View
         return selectedStepper.size();
     }
 
-    public String getSelected(){
+    public String getSelected() {
         notifyDataSetChanged();
         return selected;
     }
-    public static class ViewHolder extends RecyclerView.ViewHolder{
-    CardView cardView;
-    TextView description;
 
-    public ViewHolder(@NonNull View itemView) {
-        super(itemView);
-        cardView = itemView.findViewById(R.id.cardViewStepper);
-        description= itemView.findViewById(R.id.textStepper);
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        TextView description;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cardView = itemView.findViewById(R.id.cardViewStepper);
+            description = itemView.findViewById(R.id.textStepper);
+        }
     }
-}
 
 
 }
